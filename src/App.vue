3d522b73ae9a4597e607848a2bb6221d64b32755
Gabriel Munhak <heclypso@gmail.com>
2024-07-09 18:57:08 -0300

    Realizado desafio de exibir uma mensagem quando não existir nenhuma tarefa pendente

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,77 @@
 <script setup>
-import { reactive } from 'vue';
-import Cabecalho from './components/Cabecalho.vue'
-import Formulario from './components/Formulario.vue'
-import ListaDeTarefas from './components/ListaDeTarefas.vue'
+import { reactive, computed } from 'vue';
+import Cabecalho from './components/Cabecalho.vue';
+import Formulario from './components/Formulario.vue';
+import ListaDeTarefas from './components/ListaDeTarefas.vue';
 
-
-  const estado = reactive ({
-    filtro: 'todas',
-    tarefaTemp: '',
-    tarefas: [
-      {
-        titulo: 'Estudar ES6',
-        finalizada: false,
-      },
-      {
-        titulo: 'Estudar SASS',
-        finalizada: false,
-      },
-      {
-        titulo: "Ir para a academia",
-        finalizada: true,
-      }
-    ]
-  })
-
-  const getTarefasPendentes = () => {
-    return estado.tarefas.filter(tarefa => !tarefa.finalizada)
-  }
-
-  const getTarefasFinalizadas = () => {
-    return estado.tarefas.filter(tarefa => tarefa.finalizada)
-  }
-
-  const getTarefasFiltradas = () => {
-    const { filtro } = estado;
-
-    switch (filtro) {
-      case 'pendentes':
-        return getTarefasPendentes();
-      case 'finalizadas':
-        return getTarefasFinalizadas();
-      default:
-        return estado.tarefas;
-    }
-  }
-
-  const cadastraTarefa = () => {
-    const tarefaNova = {
-      titulo: estado.tarefaTemp,
+const estado = reactive({
+  filtro: 'todas',
+  tarefaTemp: '',
+  tarefas: [
+    {
+      titulo: 'Estudar ES6',
       finalizada: false,
-    }
-    estado.tarefas.push(tarefaNova);
-    estado.tarefaTemp = '';
+    },
+  ],
+});
+
+const getTarefasPendentes = () => {
+  return estado.tarefas.filter((tarefa) => !tarefa.finalizada);
+};
+
+const getTarefasFinalizadas = () => {
+  return estado.tarefas.filter((tarefa) => tarefa.finalizada);
+};
+
+const getTarefasFiltradas = () => {
+  const { filtro } = estado;
+
+  switch (filtro) {
+    case 'pendentes':
+      return getTarefasPendentes();
+    case 'finalizadas':
+      return getTarefasFinalizadas();
+    default:
+      return estado.tarefas;
   }
+};
+
+const cadastraTarefa = () => {
+  const tarefaNova = {
+    titulo: estado.tarefaTemp,
+    finalizada: false,
+  };
+  estado.tarefas.push(tarefaNova);
+  estado.tarefaTemp = '';
+};
+
+const todasTarefasFinalizadas = computed(() => {
+  return estado.tarefas.every((tarefa) => tarefa.finalizada);
+});
 </script>
+
 
 <template>
   <div class="container">
     <Cabecalho :tarefas-pendentes="getTarefasPendentes().length"/>
     <Formulario :trocar-filtro="evento => estado.filtro = evento.target.value" :tarefa-temp="estado.tarefaTemp" :edita-tarefa-temp="evento => estado.tarefaTemp = evento.target.value" :cadastra-tarefa="cadastraTarefa"/>
+    <div v-if="todasTarefasFinalizadas">
+    <p class="mt-3 text-center mensagem">
+      Você não possui tarefas pendentes.
+    </p>
+    </div>
     <ListaDeTarefas :tarefas="getTarefasFiltradas"/>
-</div>
+  </div>
 </template>
 
+<style>
+
+.container .mensagem{
+  background-color: #d3f0d2;
+  color: #5bad3b;
+  font-size: 20px;
+  border-radius: 3px
+}
+
+</style>
 
